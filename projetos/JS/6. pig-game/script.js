985fb3ff43f958b491a2bd272a3be1637ebe6da1
Vitor Raimundo <vitorbarbosaraimundo68@gmail.com>
2024-03-06 17:32:06 -0300

    Finalizando projeto pig game

--- projetos/JS/6. pig-game/script.js
+++ projetos/JS/6. pig-game/script.js
@@ -1,34 +1,92 @@
 'use strict';
 
+// Starting Conditions
 const dice = document.querySelector('.dice');
 const rollDice = document.querySelector('.roll-dice');
+const newGame = document.querySelector('.new-game');
+const hold = document.querySelector('.hold');
+const current0 = document.querySelector('#current-0');
+const current1 = document.querySelector('#current-1');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const score = document.querySelector('.score');
 
-rollDice.addEventListener('mousedown', e => {
+let currentValue = 0;
+let activePlayer = 0;
+let playing = true;
+const scoreArr = [0, 0];
+
+// Função para trocar de jogador
+const switchPlayer = () => {
+  document.querySelector(`#current-${activePlayer}`).textContent = 0;
+  currentValue = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  left.classList.toggle('player-active');
+  right.classList.toggle('player-active');
+};
+
+// Ao pressionar o botão ROLL DICE
+rollDice.addEventListener('mousedown', () => {
   rollDice.classList.add('effect');
+  rollDice.classList.remove('no-effect');
 });
 
+// Ao soltar o botão ROLL DICE
 rollDice.addEventListener('mouseup', () => {
-  let numberDice = Math.trunc(Math.random() * 6) + 1;
-  dice.classList.remove('hidden');
-  rollDice.classList.remove('no-effect')
-  console.log(numberDice);
-  switch (numberDice) {
-    case 1:
-      dice.src = 'dice-1.png';
-      break;
-    case 2:
-      dice.src = 'dice-2.png';
-      break;
-    case 3:
-      dice.src = 'dice-3.png';
-      break;
-    case 4:
-      dice.src = 'dice-4.png';
-      break;
-    case 5:
-      dice.src = 'dice-5.png';
-      break;
-    default:
-      dice.src = 'dice-6.png';
+  if (playing) {
+    let numberDice = Math.trunc(Math.random() * 6) + 1;
+    dice.classList.remove('hidden');
+    rollDice.classList.remove('effect');
+
+    // console.log(numberDice);
+
+    // Valor do dado
+    dice.src = `dice/dice-${numberDice}.png`;
+
+    // Checar se o dado tem valor 1
+    if (numberDice !== 1) {
+      // Adicionar o dado há pontuação atual
+      currentValue += numberDice;
+      document.querySelector(`#current-${activePlayer}`).textContent =
+        currentValue;
+    } else {
+      // Trocar com o outro player
+      switchPlayer();
+    }
   }
+});
+
+// Ao pressionar o botão HOLD
+hold.addEventListener('mouseup', () => {
+  if (playing) {
+    // 1. Adicionar a pontuação atual do jogador
+    scoreArr[activePlayer] += currentValue;
+    // score[i] = score[i] + currentValue
+    // console.log(scoreArr[activePlayer]);
+    document.querySelector(`#score-${activePlayer}`).textContent =
+      scoreArr[activePlayer];
+
+    // 3. Ver se o jogador tem pontuação >= 100
+    if (scoreArr[activePlayer] >= 20) {
+      // 4. Finish the game
+      playing = false;
+      document.querySelector(`.player${activePlayer}`).classList.add('win');
+      document
+        .querySelector(`.player${activePlayer}`)
+        .classList.remove('player-active');
+      dice.classList.add('hidden');
+    } else {
+      // 5. Trocar com o outro player
+      switchPlayer();
+    }
+  }
+});
+
+// Ao pressionar o botão NEW GAME
+newGame.addEventListener('mouseup', () => {
+  currentValue = 0;
+  activePlayer = 0;
+  playing = true;
+  scoreArr.splice(0, 2, 0, 0);
+  dice.classList.add('hidden');
 });
