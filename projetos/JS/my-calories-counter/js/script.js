75365d6e6e513250741af69a507e5185d21ceda6
Vitor Raimundo <vitorbarbosaraimundo68@gmail.com>
2024-04-02 01:23:40 -0300

    Calculo de tmb

--- projetos/JS/my-calories-counter/js/script.js
+++ projetos/JS/my-calories-counter/js/script.js
@@ -1,5 +1,33 @@
 'use strict';
 
 // Starting Conditions
+const male = document.querySelector('#male');
+const female = document.querySelector('#female');
+const height = document.querySelector('#height-info');
+const weight = document.querySelector('#weight-info');
+const age = document.querySelector('#age-info');
+const tmbResult = document.querySelector('.tmb-number-result');
+const calculate = document.querySelector('.calculate');
 
+// Reset
+const init = () => {
+};
 
+init();
+
+// Cálculo do tmb
+calculate.addEventListener('click', () => {
+  male = 66.473;
+  female = 655.0955;
+  if (sex === male) {
+    const maleCalculation = (sex, height, weight, age) => {
+      tmbResult.textContent =
+        sex + 13.7516 * weight + 5.0033 * height - 6.755 * age;
+    };
+  } else {
+    const femaleCalculation = (height, weight, age) => {
+      tmbResult.textContent =
+        sex + 9.5634 * weight + 1.8496 * height - 4.6756 * age;
+    };
+  }
+});
