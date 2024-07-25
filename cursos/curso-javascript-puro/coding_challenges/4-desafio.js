38b0ec61e50ccbbffbd0a3ab411a7db63cd5f3b3
Vitor Raimundo <vitorbarbosaraimundo68@gmail.com>
2024-07-24 21:01:05 -0300

    Super Challenge

--- cursos/curso-javascript-puro/coding_challenges/4-desafio.js
+++ cursos/curso-javascript-puro/coding_challenges/4-desafio.js
@@ -1,17 +1,62 @@
+// Crianção e estilização dos elementos
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
-
-const convertCamelCase = underscore_case => {
-  const camelCase = underscore_case.trim().split('_');
-  const perfectCamelCase =
-    camelCase[0] + camelCase[1][0].toUpperCase() + camelCase[1].slice(1);
-  document.querySelector('textarea').textContent = perfectCamelCase;
-  // return perfectCamelCase;
-};
-// underscore_case
+document.body.querySelector('button').textContent = 'click me';
+document.body.querySelector('textarea').style.height = '8rem';
+document.body.querySelector('textarea').style.width = '15rem';
+// document.body.querySelector('textarea').value = `underscore_case
 //  first_name
 // Some_Variable
-//  calculate_AGE
-// delayed_departure
+//   calculate_AGE
+// delayed_departure`;
 
-document.querySelector('button').addEventListener('click', convertCamelCase);
+// Função para converter snake_case para camelCase
+const convert = () => {
+  // inputText do textarea e arrays das linhas separadas
+  const inputText = document.querySelector('textarea').value;
+  const rows = inputText.toLowerCase().split('\n');
+
+  // Output do array final
+  let outputRows = [];
+
+  // For para percorrer os elementos do array de rows
+  for (const [i, line] of rows.entries()) {
+    const trimmedRows = line.trim();
+    const camelCaseRows =
+      trimmedRows.slice(0, trimmedRows.lastIndexOf('_')) +
+      trimmedRows.slice(trimmedRows.lastIndexOf('_') + 1)[0].toUpperCase() +
+      trimmedRows.slice(trimmedRows.lastIndexOf('_') + 2);
+
+    // Colocando os checks dentro de cada linha e transformando de volta em array para os resultados não se sobreporem
+    outputRows.push(camelCaseRows.padEnd(20) + '✅'.repeat(i + 1));
+  }
+
+  // Output do Resultado final
+  const output = outputRows.join('\n');
+
+  // Aplicando o output dentro do textarea e desativando o botão
+  document.querySelector('textarea').value = output;
+  document.querySelector('button').disabled = true;
+  console.log(output);
+};
+
+// Evento para o botão onde chamará a função acima
+document.querySelector('button').addEventListener('click', convert);
+
+/*
+Resultado:
+underscoreCase    ✅
+firstName         ✅✅
+someVariable      ✅✅✅
+calculateAge      ✅✅✅✅
+delayedDeparture  ✅✅✅✅✅
+*/
+
+/*
+Recebido:
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+*/
